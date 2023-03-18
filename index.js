f217967bd6d42277874a90c3296f9c1db5bca4ff
Vadym Shevchenko <vdshvch@gmail.com>
2023-03-18 20:34:02 +0200

    inti

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const bodyParser = require('body-parser');
+const ftp = require('basic-ftp');
+const { Buffer } = require('node:buffer');
+
+// nodejs 10.17.0 and up
+const { Readable } = require('stream');
+
+
+const app = express();
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.post('/upload', async (req, res) => {
+  try {
+    // console.log(req.body);
+    // {
+    //     "orderId": ,
+    //     "lineItems": [
+    //         {
+    //             "_sticker_image": "",
+    //             "_sticker_product_sku": ""
+    //         }
+    //     ]
+    // }
+   
+
+    const orderId = req.body.orderId;
+    const arr = req.body.lineItems.filter(el => el._sticker_image);
+
+
+
+
+    const client = new ftp.Client();
+    client.ftp.verbose = true;
+
+    await client.access({
+        host: 'ftp.alekspartners.com',
+        user: 'dmitrii@alekspartners.com',
+        password: 'O{lN7%W[0zb%',
+    });
+
+
+
+
+    for(let i=0; i<arr.length; i++) {
+      const base64Data = arr[i]._sticker_image.replace(/^data:image\/png;base64,/, "");
+      const decodedImage = Buffer.from(base64Data, 'base64');
+      const stream = Readable.from(decodedImage);
+
+      const remoteDir = '/00-RECEIPT/05-ETIKETS';
+      await client.cd(remoteDir);
+
+
+      // await client.uploadFrom(decodedImage, `/00-RECEIPT/05-ETIKETS/order-${orderId}__sku-${arr[i]._sticker_product_sku}.png`);
+      await client.uploadFrom(stream, `order-${orderId}__sku-${arr[i]._sticker_product_sku}.jpg`);
+    }
+
+    await client.close();
+
+    res.status(200).send('Images uploaded to FTP server');
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error uploading image to FTP server');
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening on port 3000');
+});
